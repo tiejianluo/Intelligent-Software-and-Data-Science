cb88b45fa2d99f64d54156a55cf369f85b4fe6eb
tiejianluo <tiejianluo@users.noreply.github.com>
2016-09-15 02:47:30 +0800

    Update application_controller.rb
    This is a test for a new app. ---ltj

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-  	render html: "hello,world!,你好！"
+  	render html: "1  ---hello,world!,你好！"
+  	render html: "2   ---hello,world!,你好！"
+  	render html: "3   --hello,world!,你好！"
   end
 end
